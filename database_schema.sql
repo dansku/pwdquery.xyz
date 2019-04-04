a6f59b9f0b104e57e5125ef8ea11192921cec009
Daniel Andrade <daniel@danielandrade.net>
2019-04-04 17:24:33 +0100

    Add db schema

---
+++ database_schema.sql
@@ -0,0 +1,243 @@
+CREATE TABLE pwd_database.a (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.a (email);
+CREATE TABLE pwd_database.b (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.b (email);
+CREATE TABLE pwd_database.c (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.c (email);
+CREATE TABLE pwd_database.d (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.d (email);
+CREATE TABLE pwd_database.e (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.e (email);
+CREATE TABLE pwd_database.f (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.f (email);
+CREATE TABLE pwd_database.g (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.g (email);
+CREATE TABLE pwd_database.h (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.h (email);
+CREATE TABLE pwd_database.i (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.i (email);
+CREATE TABLE pwd_database.j (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.j (email);
+CREATE TABLE pwd_database.k (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.k (email);
+CREATE TABLE pwd_database.l (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.l (email);
+CREATE TABLE pwd_database.m (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.m (email);
+CREATE TABLE pwd_database.n (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.n (email);
+CREATE TABLE pwd_database.o (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.o (email);
+CREATE TABLE pwd_database.p (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.p (email);
+CREATE TABLE pwd_database.q (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.q (email);
+CREATE TABLE pwd_database.r (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.r (email);
+CREATE TABLE pwd_database.s (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.s (email);
+CREATE TABLE pwd_database.t (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.t (email);
+CREATE TABLE pwd_database.u (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.u (email);
+CREATE TABLE pwd_database.v (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.v (email);
+CREATE TABLE pwd_database.w (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.w (email);
+CREATE TABLE pwd_database.x (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.x (email);
+CREATE TABLE pwd_database.y (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.y (email);
+CREATE TABLE pwd_database.z (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.z (email);
+CREATE TABLE pwd_database.other (
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(50) NOT NULL,
+	password varchar(4) NOT NULL,   pnum TINYINT NOT NULL,
+	CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='utf8_general_ci,COERCIBLE';
+CREATE INDEX email USING BTREE ON pwd_database.other (email);
